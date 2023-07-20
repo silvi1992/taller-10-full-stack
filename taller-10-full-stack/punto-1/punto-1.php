a37c45b93e234245a4ac69a4fe11fe537d0aed2a
silvia1992 <marmolejosilvia918@gmail.com>
2023-07-20 13:51:58 -0500

    primer commit

---
+++ taller-10-full-stack/punto-1/punto-1.php
@@ -0,0 +1,22 @@
+<?php
+// CONSTANTS
+const PI = 3.14159;
+define("WELCOME_MESSAGE", "Bienvenidos");
+
+//VARIABLES
+$firstName = "Silvia";
+$lastName = "Mosquera";
+
+echo PI . "\n";
+echo WELCOME_MESSAGE . "\n";
+echo $firstName . " " . $lastName . "\n";
+
+$firstName = "Leonor";
+$lastName = "Marmolejo";
+
+echo "-----------------\n";
+
+echo PI . "\n";
+echo WELCOME_MESSAGE . "\n";
+echo $firstName . " " . $lastName . "\n";
+?>
